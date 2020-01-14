e694a1c8b94549ff6ed6be5c8d4c2f86343533dc
sooshe <sooshe@sooshebot.com>
2020-01-14 17:58:53 -0600

    debug fetchfollowers axios call

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super();
     this.state = {
       users: [],
-      followers:''
+      followers: ""
     };
   }
 
@@ -28,22 +28,22 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.users !== prevState.users) {
       console.log("inside first if?");
-      if (this.state.followers === '') {
-
+      if (this.state.followers === "") {
+        console.log('inside second if')
         axios
-          .get('https://api.github.com/users')
+          .get("https://api.github.com/users")
           .then(res => {
             // res.data.message
             this.setState({
               users: res.data,
               followers: "users"
             });
-            console.log('cDU res is', res);
+            console.log("cDU res is", res);
           })
-          .catch(err => console.log('cDU error is', err));
-        }
+          .catch(err => console.log("cDU error is", err));
+      }
     }
-  };
+  }
 
   handleChanges = e => {
     this.setState({
@@ -53,17 +53,28 @@ class App extends Component {
 
   fetchFollowers = e => {
     e.preventDefault();
-  }
-
-
+    axios
+      .get(`https://api.github.com/users/${this.state.followers_url}`)
+      .then(res => {
+        
+        this.setState({
+          users: res.data
+        });
+        console.log(` followers are`, res);
+      })
+      .catch(err => console.log("fetchFollowers error is", err));
+  };
 
   render() {
     return (
       <div className="App">
+        <h1>Github Users</h1>
+        {/* <input type='text' value={this.state.followers} onChange={this.handleChanges} /> */}
         {this.state.users.map(user => {
           return (
             <div className="users" key={user.id}>
-              <UserCard user={user}/>
+              <UserCard user={user} />
+              <button onClick={this.fetchFollowers}>Fetch Followers</button>
             </div>
           );
         })}

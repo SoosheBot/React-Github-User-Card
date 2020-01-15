69c35debdd6c86a1a9b735255f6b0dd22389b5b4
sooshe <sooshe@sooshebot.com>
2020-01-14 18:23:35 -0600

    update cDM in users

--- usercard/src/components/User.js
+++ usercard/src/components/User.js
@@ -3,6 +3,26 @@ import axios from 'axios';
 import UserCard from './UserCard';
 
 class User extends Component {
+    state = {
+        username: []
+    };
+
+    componentDidMount() {
+        axios
+        .get("https://api.github.com/users/sooshebot")
+        .then(res => {
+            this.setState({
+                img: res.data.avatar_url,
+                username: res.data.login  
+            });
+            console.log('user cDM', res);
+        })
+        .catch (err => console.log ('user cDM error:', err));
+    }
+
+    render() {
+        return();
+    }
 
 }
 
--- usercard/src/components/UserCard.js
+++ usercard/src/components/UserCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-const UserCard = ({img, login}) => {
+const UserCard = ({img, username}) => {
     return (
         <div className="usercard">
             <img width='150' src={img} alt='user' />
-            <h3 className="username">Username: {login}</h3>
+            <h3 className="username">Username: {username}</h3>
         </div>
 
       );
